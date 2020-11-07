c449f0071db31ae0bbd245eef0ea62dc23c904c6
cameron <cameron.leverett@yahoo.com>
2020-11-07 15:44:44 -0800

    building controllers for --api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::PostsController < ApplicationController
+  def index
+    posts = Post.all
+    render json: posts
+    end  
+  
+    def create
+      post = Post.new(post_params)
+      if post.save
+        render json: post
+      else 
+        render json: {error: 'Error creating account'}
+    end
+  
+    def show
+      post = Post.find(params[:id])
+      render json: post
+    end
+    
+    def update
+      
+    end
+  
+    def destroy
+      post = Post.find(params[:id])
+      post.destroy
+    end
+  
+    private
+  
+    def post_params
+      params.require(:post).permit(:user_id, :img, :text)
+    end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,31 @@ class Api::V1::UsersController < ApplicationController
   render json: users
   end  
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else 
+      render json: {error: 'Error creating account'}
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+  
+  def update
+    
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
 end
