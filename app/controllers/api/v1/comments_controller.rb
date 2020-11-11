04d0c8fb2fb654355c0abb4b77e4728d898ac6fd
cameron <cameron.leverett@yahoo.com>
2020-11-10 16:15:19 -0800

    resource comments for posts

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,12 @@
-class CommentsController < ApplicationController
+class Api::V1::CommentsController < ApplicationController
+  before_action :set_user
+
+  def index
+    comments = @post.comments
+    render json: comments
+  end
+
+  def set_user
+    @post = Post.find(params[:post_id])
+  end
 end
