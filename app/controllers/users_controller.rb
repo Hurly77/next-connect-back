40d48128702d00ddd6fd006705306987da2cd533
cameron <cameron.leverett@yahoo.com>
2020-11-13 16:01:21 -0800

    don't need anymore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,17 @@
 class UsersController < ApplicationController
 
-  def index
-  users = User.all
-  render json: UserSerializer.new(users)
-  end  
-
   def create
     user = User.new(user_params)
     if user.save
-      render json: UserSerializer.new(user)
+      render json: user
     else 
-      render json: {error: 'Error creating account'}
+      render json: {status: "not successful"}
     end
-  end
-
-  def show
-    user = User.find(params[:id])
-    render json: UserSerializer.new(user)
-  end
-  
-  def update
-    
-  end
-
-  def destroy
-    user = User.find(params[:id])
-    user.destroy
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
