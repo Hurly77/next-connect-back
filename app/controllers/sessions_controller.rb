4344cda110efbec9305dd169a6434ecf6920c3d7
cameron <cameron.leverett@yahoo.com>
2020-11-17 19:12:35 -0800

    having trouble sending data from my app to api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,9 @@ class SessionsController < ApplicationController
   include CurrentUserConcern
 
   def create
-    user = User
-          .find_by(email: params[:user][:email])
-          .try(:authenticate, params[:user][:password])
+    user = User.find_by(email: params[:user][:email])
 
-  if user 
+        if user && user.authenticate(params[:user][:password]) 
     session[:user_id] = user.id
     render json: {
       status: :created,
