4243cfae3bba171286173e560a1f757daac2aa18
cameron <cameron.leverett@yahoo.com>
2020-11-30 13:23:09 -0800

    added resoures for my search controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :posts
       resources :comments
+      resources :search
     end
   end
   resources :sessions, only: [:create]
