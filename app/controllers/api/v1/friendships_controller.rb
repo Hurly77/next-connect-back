251d1c264eefaa159703358565cd77d782f600c6
cameron <cameron.leverett@yahoo.com>
2020-12-02 20:21:08 -0800

    testing friendship controller actions

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -1,50 +1,23 @@
 class Api::V1::FriendshipsController < ApplicationController
   def create
-    friendship = Friendship.new(friendships_params)
-    if !Friendship.requested?(active_user, passive_user)
-      pending_friends = Friendship.friends_pending(active_user),
-      friendship.save
-      render json: {
-        pending_friends: pending_friends
-      }
-
+    if !Friendship.requested?(params[:active_user_id], params[:passive_user_id])
+      friendship = Friendship.create(friends_params)
+      render json: pending_friends
     else
-      pending = Friendship.friends_pending(active_user) 
-      render json: {pending_friends: pending}
-    end
-  end
-
-  def update
-    if params[:status] == "ACCEPTED"
-      status Friendship.accept_request(passive_user, active_user)
-      render json: {
-        friends: Friendship.all_friends(active_user.id),
-      }
-    else
-      render json: {
-        error: "SOMETHING WENT WRONG",
-      }
+      pending = Friendship.friends_pending(params[:active_user_id]) 
+      render json: pending
     end
   end
 
   def destroy
-    friendship = Friendship.find_by(passive_user_id: params[:passive_user])
+    friendship = Friendship.find_by(passive_user_id: params[:passive_user_id])
     if params[:status] === "DENIED"
       friendship.destroy_all
     end
   end
 
-  def passive_user
-    return User.find_by(id: params[:passive_user_id])
-  end
-
-  def active_user
-    return User.find_by(id: params[:active_user_id])
-  end
-
   private
-
-  def friendships_params
+  def friends_params
     params.require(:friendship).permit(:active_user_id, :passive_user_id, :status)
   end
 end
