20c79b4395a20e180998b307109c517fc5617534
cameron <cameron.leverett@yahoo.com>
2020-12-02 20:20:27 -0800

    added a relation so users and belong to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,21 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
   has_many :comments
-  has_many :connections
 
-  def self.query(q) 
-     User.where("first_name LIKE ?", "%#{q}%")
+  has_many :active_friendships,
+           foreign_key: :active_user_id,
+           class_name: :Friendship,
+           dependent: :destroy
+
+  has_many :passive_friendships,
+           foreign_key: :passive_user_id,
+           class_name: :Friendship,
+           dependent: :destroy
+
+  has_many :active_friends, through: :active_friendships, source: :active_user
+  has_many :passive_friends, through: :passive_friendships, source: :passive_user
+
+  def self.query(q)
+    User.where("first_name LIKE ?", "%#{q}%")
   end
 end
