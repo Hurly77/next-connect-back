1fcd9e5456e223f48c0100fe8bfd0355807217dd
cameron <cameron.leverett@yahoo.com>
2020-12-03 21:17:23 -0800

    removed stuff that I don't need anymore

--- app/controllers/api/v1/Users_controller.rb
+++ app/controllers/api/v1/Users_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::UsersController < ApplicationController
+  include CurrentUserConcern
+
+
   def index
     users = User.query(params[:query])
     if users
@@ -6,6 +9,22 @@ class Api::V1::UsersController < ApplicationController
     else 
       render json: {status: 'not success'}
     end
+  end
+
+  def update
+      user = User.find_by(id: params[:user][:id])
+      user.update(
+        first_name: params[:user][:first_name], 
+        last_name: params[:user][:last_name], 
+        email: params[:user][:email], 
+        avatar: params[:user][:avatar], 
+        work: params[:user][:work],
+        education: params[:user][:education],
+        relationship: params[:user][:relationship],
+        lives: params[:user][:lives],
+        from: params[:user][:from],
+        joined: params[:user][:joined],
+      )
   end
 
   def show
