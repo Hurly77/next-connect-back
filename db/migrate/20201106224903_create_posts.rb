6a116ec50b80c52e10bf6332eb7b4fa4f64c3de8
cameron <cameron.leverett@yahoo.com>
2020-12-03 21:18:56 -0800

    added more attributes so user will see who posted

--- db/migrate/20201106224903_create_posts.rb
+++ db/migrate/20201106224903_create_posts.rb
@@ -2,8 +2,11 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.integer :user_id
-      t.string :img
+      t.string :users_full_name
+      t.string :users_avatar
+      t.string :img, default: ''
       t.string :text
+      t.date :posted_at, default: DateTime.now.to_date
 
       t.timestamps
     end
