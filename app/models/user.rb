f59fde6d8d1829b63bae0a7d932df0f0764ee2b3
cameron <cameron.leverett@yahoo.com>
2020-12-28 16:05:40 -0800

    added query methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,10 +20,22 @@ class User < ApplicationRecord
     User.where("first_name LIKE ?", "%#{q}%")
   end
 
-  def self.requests(id)
-    requests = self.find_by(id: id).passive_friendships
-    r_list = requests.map{|r| r.active_user_id }
-    req_for_user = r_list.map{|id| self.find_by(id: id)}
-    return req_for_user
+  def pending_friends
+    requests = self.active_friendships
+    r_list = requests.map{|u| u.status === "PENDING" ? User.find_by(id: u.passive_user_id) : []}.flatten
+    return r_list
+  end
+
+  def requests
+    requests = self.passive_friendships
+    r_list = requests.map{|r| r.status === "PENDING" ? User.find_by(id: r.active_user_id) : []}.flatten
+    return r_list
+  end
+
+
+  def friends
+    friends = self.active_friendships
+    f_list = friends.map{|f| f.status === "FRIENDS" ? User.find_by(id: f.passive_user_id) : []}.flatten
+    return f_list
   end
 end
