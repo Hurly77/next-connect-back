3aae400c3f93c16dfac9dc7fc04cbad339de1757
cameron <cameron.leverett@yahoo.com>
2021-01-03 17:02:13 -0800

    for got to add key id: to find_by and removed pry

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::FriendshipsController < ApplicationController
 
   def friend_request 
-    user = User.find_by(params[:active_user_id])
+    user = User.find_by(id: params[:active_user_id])
     if !Friendship.requested?(params[:active_user_id], params[:passive_user_id])
       Friendship.find_or_create_by(friends_params)
       render json: {pending_friends: user.pending_friends}
@@ -12,7 +12,6 @@ class Api::V1::FriendshipsController < ApplicationController
 
   def accept
     user = User.find_by(id: params[:active_user_id])
-    binding.pry
     if Friendship.requested?(params[:active_user_id], params[:passive_user_id])
        Friendship.accept_request(params[:active_user_id], params[:passive_user_id])
       render json: {friends: user.friends} 
