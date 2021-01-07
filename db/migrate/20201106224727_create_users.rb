a0881b15ca3729fbf65c82cf6bc5313cd4cf231b
cameron <cameron.leverett@yahoo.com>
2021-01-07 12:26:03 -0800

    added attribute for storing image url

--- db/migrate/20201106224727_create_users.rb
+++ db/migrate/20201106224727_create_users.rb
@@ -1,19 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :photo_url, null: true
       t.string :first_name
       t.string :last_name
       t.string :email
       t.string :password_digest
-      t.string :avatar, default: Faker::Avatar.image
       t.string :work, default: ''
       t.string :education, default: ''
       t.string :relationship, default: ''
       t.string :lives, default: ''
       t.string :from, default: ''
-      t.date :joined, default: DateTime.now.to_date
-
       t.string :c_id
+      t.date :joined, default: DateTime.now.to_date
       
       t.timestamps
     end
