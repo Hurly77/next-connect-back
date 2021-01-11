e1e995811df96dd1fc1ffa1db605140eb13b5264
cameron <cameron.leverett@yahoo.com>
2021-01-10 20:30:35 -0800

    added query a post url for photo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
-  
+  has_many :post_photos
+  has_many_attached :photos
+
+
+  def post_urls(photos)
+    photos_array = []
+    return photos.map do |photo| 
+      url = url_for(photo)
+    end
+  end
+
 end
