fdde06b91413deff5746008e47034d2152d1a521
cameron <cameron.leverett@yahoo.com>
2021-01-10 20:29:46 -0800

    added a photos controller to store photos

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -5,26 +5,23 @@ class Api::V1::PhotosController < ApplicationController
     if params[:file]
       @user.avatar.attach(params[:file])
       photo = url_for(@user.avatar)
-    elsif params[:camera]
-      blob = ActiveStorage::Blob.create_after_upload!(
-        io: StringIO.new((Base64.decode64(params[:camera].split(",")))),
-        filename: "user.png",
-        content_type: "image/png",
-      )
-      @user.avatar.attach(blob)
-      photo = url_for(@user.avatar)
-    else
-      photo = photo_params[:photo]
-    end
     if @user.update(photo_url: photo)
       @user.update_avatar(photo)
       render json: @user 
     end
   end
 
+  if params[:banner]
+    @user.banner.attach(params[:banner])
+    photo = url_for(@user.banner)
+    if @user.update(banner_url: photo)
+      render json: @user
+      end
+    end
+  end
+
   
   def show
-    binding.pry
     user = User.find_by(id: params[:id])
     render json: user
   end
