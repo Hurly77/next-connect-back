ba844fd8c143c284add924a6f2f6e8deecf2523f
cameron <cameron.leverett@yahoo.com>
2021-01-15 22:13:43 -0800

    added user id for post photos

--- db/migrate/20210110005819_create_post_photos.rb
+++ db/migrate/20210110005819_create_post_photos.rb
@@ -2,6 +2,7 @@ class CreatePostPhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :post_photos do |t|
       t.string :url
+      t.integer :user_id
       t.integer :post_id
 
       t.timestamps
