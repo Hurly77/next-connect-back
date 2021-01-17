3fa259f34c931c6e2931b00d4227336f74d6d5c8
cameron <cameron.leverett@yahoo.com>
2021-01-16 23:00:43 -0800

    trying some things out didn't work

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,17 +3,4 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :post_photos
 
-
-  def make_photos(photos)
-        
-  end
-
-
-  def post_urls(photos)
-    photos_array = []
-    return photos.map do |photo| 
-      url = url_for(photo)
-    end
-  end
-
 end
