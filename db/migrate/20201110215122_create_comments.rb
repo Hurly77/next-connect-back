5dd6b44ae26bb1923df4e3a547c6c566405948f4
cameron <cameron.leverett@yahoo.com>
2021-01-16 23:02:00 -0800

    added attributs to comments

--- db/migrate/20201110215122_create_comments.rb
+++ db/migrate/20201110215122_create_comments.rb
@@ -2,6 +2,9 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :text
+      t.string :users_avatar
+      t.string :users_full_name
+      t.date :posted_at, default: DateTime.now.to_date
       t.integer :user_id
       t.integer :post_id
 
