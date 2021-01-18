36bc23799ad8408c73c2efa357819792b59082cf
cameron <cameron.leverett@yahoo.com>
2021-01-17 20:09:55 -0800

    added info instance method to return user

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::CommentsController < ApplicationController
+  def index
+  end
+
   def create
-    comment = Comment.create(comment_params)
-    
+    Comment.create(comment_params)
+    post = Post.find_by(id: params[:comment][:post_id])
+    render json: {post: post, photos: post.photos, comments: post.comments}
   end
 
   private 
   
   def comment_params
-    params.require(comment).permit!
+    params.require(:comment).permit(
+      :post_id, 
+      :user_id, 
+      :users_avatar, 
+      :users_full_name, 
+      :text)
   end
 end
