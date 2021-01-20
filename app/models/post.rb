41fab583508279630f218c627aae360763fa2c93
cameron <cameron.leverett@yahoo.com>
2021-01-19 17:49:05 -0800

    added like inst method

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,9 @@ class Post < ApplicationRecord
   def photos
     PostPhoto.where("post_id = ?", id).pluck(:url)
   end
+
+  def liked(id)
+    like = Like.where("post_id = ? AND user_id = ?", self.id, id)
+    return like.length > 0
+  end
 end
