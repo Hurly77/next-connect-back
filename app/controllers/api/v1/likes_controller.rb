1c9d3b8b25884fdc52fececbad0f621bbaaea4ba
cameron <cameron.leverett@yahoo.com>
2021-01-19 17:48:25 -0800

    added a controller for liking a post

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::LikesController < ApplicationController
+  before_action :set_post, only [:create, :destroy]
+
+  def create
+    if !@post.liked?
+      render json: {return true}
+    else
+      render json: {}
+    end
+  end
+
+  def destroy
+    
+  end
+
+
+  def set_post 
+    @post = Post.find_by(id: params[:like][:post_id])
+
+    @user = User.find_by(id: params[:like][:user_id])
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:user_id, :post_id)
+  end
 end
