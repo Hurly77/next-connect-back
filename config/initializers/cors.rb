a78fbc0ac2a0d4d475a01d0e4f973ccd104fd4e6
cameron <cameron.leverett@yahoo.com>
2021-01-21 15:01:42 -0800

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins '*'
+    origins 'http://localhost:3001'
 
     resource '*',
       headers: :any,
