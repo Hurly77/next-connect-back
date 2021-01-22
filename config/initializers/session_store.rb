acb0a5d775910f5ef21ec08e790ffaeabdef290d
cameron <cameron.leverett@yahoo.com>
2021-01-22 14:54:34 -0800

    sessin update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 Rails.application.config.session_store :cookie_store, {
   key: "_next_connect_back", 
-  domain: :all,
+  domain: "next-connect-back.herokuapp.com",
   same_site: :none,
   secure: true,
   tld_length: 2
