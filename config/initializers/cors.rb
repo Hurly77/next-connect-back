409569a0d069bb880d16fede59c6683ceba7dca1
cameron <cameron.leverett@yahoo.com>
2021-01-21 18:08:49 -0800

    update to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://nextconnect.surge.sh'
 
     resource '*',
       headers: :any,
